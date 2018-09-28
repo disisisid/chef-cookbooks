9699be31672a91799cb5cecf120e1bb793f71543
disisisid <sidnshady@gmail.com>
2018-09-29 06:08:53 +0800

    tomcat-cookbook: fetch and extract tomcat tarball, add tomcat user + group

--- tomcat/recipes/default.rb
+++ tomcat/recipes/default.rb
@@ -4,6 +4,26 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-# execute "sudo yum install java-1.8.0-openjdk-devel"
+# sudo yum install java-1.8.0-openjdk-devel
 package 'java-1.8.0-openjdk-devel'
 
+# sudo groupadd tomcat
+group 'tomcat'
+
+# sudo useradd -M -s /bin/nologin -g tomcat -d /opt/tomcat tomcat
+user 'tomcat' do
+	manage_home false
+	shell '/bin/nologin'
+	group 'tomcat'
+	home '/opt/tomcat'
+end
+
+# wget http://apache.mirrors.ionfish.org/tomcat/tomcat-9/v9.0.12/bin/apache-tomcat-9.0.12.tar.gz
+remote_file 'apache-tomcat-9.0.12.tar.gz' do
+	source 'http://apache.mirrors.ionfish.org/tomcat/tomcat-9/v9.0.12/bin/apache-tomcat-9.0.12.tar.gz'
+end
+
+# sudo mkdir /opt/tomcat
+directory '/opt/tomcat'
+
+execute 'tar xvf apache-tomcat-9*tar.gz -C /opt/tomcat --strip-components=1'
--- tomcat/test/integration/default/default_test.rb
+++ tomcat/test/integration/default/default_test.rb
@@ -5,3 +5,18 @@ end
 describe package('java-1.8.0-openjdk-devel') do
 	it { should be_installed }
 end
+
+describe group('tomcat') do
+	it { should exist }
+end
+
+describe user('tomcat') do
+	it { should exist }
+	its('group') { should eq 'tomcat' }
+	its('home') { should eq '/opt/tomcat' }
+end
+
+describe file('/opt/tomcat') do
+	it { should exist }
+	it { should be_directory }
+end
